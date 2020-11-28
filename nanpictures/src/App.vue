ae291acf0d3a5caa9d3eaf70ff3feddd15b813f9
Fernando Butzke <63071834+nandobutzke@users.noreply.github.com>
2020-11-28 20:02:20 -0200

    Initial commit

---
+++ nanpictures/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ title}}</h1>
+
+    <ul>
+      <li v-for="photo of photos">
+        <img :src="photo.url" :alt="photo.titulo">
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+      title: 'Alurapic', 
+      photos: [
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'cachorro'
+        },
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'Cachorrão'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
